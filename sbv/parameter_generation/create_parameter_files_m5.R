e437d45cf905f0a8effe2125fc7a7b08e3dde5db
Belinda <belindalombard@gmail.com>
2023-08-04 18:15:57 +0200

    Parameter file

--- sbv/parameter_generation/create_parameter_files_m5.R
+++ sbv/parameter_generation/create_parameter_files_m5.R
@@ -9,41 +9,47 @@ target <- tail(.args,1)
 #Create parameter files for method 5 sensitivity analysis
 method <- 5
 
-pscale_min = 1
-pscale_max = 3
-pscale_steps = 0.5
+pscale = c(1, 1.2, 1.5)
 
-steep = c(0.0001, 0.0002, 0.00005, 0.00015)
+steep = c(0.00005, 0.0001)
 
-xm_min = 10000
+xm_min = 30000
 xm_max = 50000
-xm_steps = 20000
+xm_steps = 10000
 
-pobs_1_min = c(0.1, 0.05, 0.025)
-pobs_1_max = c(0.1, 0.2, 0.3)
+pobs_1_min = c(0.1, 0.2, 0.3, 0.4, 0.5)
+pobs_1_max = c(0.1, 0.2, 0.3, 0.4, 0.5)
+pobs_2_min = c(0.1, 0.2, 0.3, 0.4, 0.5)
+pobs_2_max = c(0.1, 0.2, 0.3, 0.4, 0.5)
+
 
 
 parameters <- expand.grid(
-  pscale = seq(pscale_min, pscale_max, pscale_steps)
-  , xm=seq(xm_min, xm_max, xm_steps)
+  xm=seq(xm_min, xm_max, xm_steps)
   , pobs_1_min=pobs_1_min
   , pobs_1_max=pobs_1_max
-  , multiplier = c(1,2,3)
+  , pobs_2_min=pobs_2_min
+  , pobs_2_max=pobs_2_max
   , steep=steep
+  , pscale = pscale
 )
 
-parameters$pobs_2_min <- parameters$multiplier * parameters$pobs_1_min
-parameters$pobs_2_max <- parameters$multiplier * parameters$pobs_1_max
 
 final <- as.data.frame(parameters)
 
 save_params <- data.frame()
 
-for (i in 1:nrow(final)) { 
-  row <- final[i, ]
-  if ( row$pobs_1_max != row$pobs_1_min && row$pobs_2_max!=row$pobs_2_min )
-    save_params <- rbind(save_params, row)
-}
+
+parameters <- subset(parameters, pobs_1_max > pobs_1_min)
+parameters <- parameters[parameters$pobs_2_min >= parameters$pobs_1_min, ]
+parameters <- parameters[parameters$pobs_2_max >= parameters$pobs_1_max, ]
+parameters <- parameters[parameters$pobs_2_max > parameters$pobs_2_min, ]
+
+
+save_params <- parameters
+
+rownames(save_params) <- NULL
+
 
 save(save_params, file = target)
 
