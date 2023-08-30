890c506c2fb27460424c1726b1baed7bb02dd91a
Belinda <belindalombard@gmail.com>
2023-08-30 16:31:18 +0200

    Method 2 fit through file fixed

--- sbv/method_2_analysis/method_2_arrayjob_fit_through.R
+++ sbv/method_2_analysis/method_2_arrayjob_fit_through.R
@@ -99,9 +99,10 @@ days_diff[days_diff>=0] <- 0
 days_diff[days_diff<0] <- 1
 
 conseq_diff <- frollsum(days_diff, 10, fill =0)
+conseq_diff_5 <- frollsum(days_diff, 5, fill =0)
 
 date_first_below_10 <- which(conseq_diff==10)[1]
-date_first_below_5 <- which(conseq_diff==5)[1]
+date_first_below_5 <- which(conseq_diff_5==5)[1]
 
 #Date first above
 days_diff_above <-  eri_ma[eri_ma$date<=fit_through,]$upp_reinf - ts[ts$date<= fit_through,]$ma_reinf
@@ -109,10 +110,10 @@ days_diff_above[days_diff_above>=0] <- 0
 days_diff_above[days_diff_above<0] <- 1
 
 conseq_diff <- frollsum(days_diff_above, 10, fill =0)
-
+conseq_diff_5 <- frollsum(days_diff_above, 5, fill =0)
 
 date_first_above_10 <- which(conseq_diff==10)[1]
-date_first_above_5 <- which(conseq_diff==5)[1]
+date_first_above_5 <- which(conseq_diff_5==5)[1]
 
 
 #Proportion outside
@@ -133,7 +134,7 @@ results <- list( pobs_2=parameters.r$pobs_2[i]
 
 #Save results
 dir.create(paste0("sbv/raw_output/m",method))
-saveRDS(results, file=paste0("sbv/raw_output/m",method,"/results_", parameters.r$index[i],".RDS"))
+saveRDS(results, file=paste0("sbv/raw_output/m",method,"/", seed_batch,"/results_", parameters.r$index[i],".RDS"))
 
 eri <- sri_long[, .(exp_reinf = median(value)
                     , low_reinf = quantile(value, 0.025, na.rm = TRUE)
@@ -186,6 +187,6 @@ inc_reinf <- (plot_sim(ts, eri, eri_ma)
               + ggtitle(wrap_title(list_text))
 )
 
-dir.create(paste0("sbv/raw_output/m",method,"/plots"))
-ggsave(inc_reinf, filename = paste0("sbv/raw_output/m",method,"/plots/sim_plot_",parameters.r$index[i],".png"), width = 6, height = 3)
+dir.create(paste0('sbv/raw_output/m', method, '/', seed_batch,'/plots'))
+ggsave(inc_reinf, filename = paste0("sbv/raw_output/m",method,"/",seed_batch, "/plots/sim_plot_",parameters.r$index[i],".png"), width = 6, height = 3)
 
