08795cc5ee310e6ed9133e7455446291c0dfabe4
Belinda <belindalombard@gmail.com>
2023-09-14 11:27:51 +0200

    update plot scripts

--- sbv/third_infections/third_infections_plots.R
+++ sbv/third_infections/third_infections_plots.R
@@ -1,35 +1,6 @@
 library(ggplot2)
 library(ggtext)
-# Assuming you have a ts frame called 'ts' with 'date' and 'ma_third' columns
-
-# Convert 'date' column to a Date object
-ts$date <- as.Date(ts$date)
-
-
-# Create the ggplot with the d--esired aesthetics
-ggplot(ts, aes(x = date, y = ma_reinf)) +
-  geom_line() +
-  scale_x_date(date_labels = "%b %Y", date_breaks = "6 months") +
-  labs(x = "Date", y = "Third") +
-  theme_minimal() + 
-  geom_vline(xintercept = as.Date(fit_through), linetype = "dashed", color = "blue") +
-  geom_vline(xintercept = as.Date(wave_split), linetype = "dashed", color = "red") +
-  geom_line(aes(y=observed, color='pink', width=0.1))
-
-
-#actual
-data <- readRDS('data/ts_data_for_analysis.RDS')
-# Create the ggplot with the d--esired aesthetics
-ggplot(data, aes(x = date, y = ma_third)) +
-  geom_line() +
-  scale_x_date(date_labels = "%b %Y", date_breaks = "6 months") +
-  labs(x = "Date", y = "Third") +
-  theme_minimal() + 
-  geom_vline(xintercept = as.Date(fit_through), linetype = "dashed", color = "blue") +
-  geom_vline(xintercept = as.Date(wave_split), linetype = "dashed", color = "red") +
-  geom_line(aes(y=third, color='pink', width=0.1))
-
-
+library(patchwork)
 
 final_RDS_l2 <- readRDS(paste0('sbv/third_infections/l2_combined_results.RDS'))
 
@@ -39,52 +10,83 @@ styling_layers <-
     theme(panel.border = element_rect(colour = "black", size = 0.25)
           , panel.grid.minor = element_blank()) 
     , theme_minimal() 
-    , scale_colour_brewer(palette = "Set2") 
+    #, scale_colour_brewer(palette = "Set2") 
     , scale_fill_gradientn(colours = rev(colorspace::terrain_hcl(100)))
   )
 
 
-final_RDS <- getAllResults('third', 'sbv/third_infections/output/ml2third/')
-load('utils/cleanup_methods.RData')
-summarised_results <- final_RDS %>% 
-                      group_by(pscale)  %>%
-                      summarise(
-                        convergence = proportion_convergence_three(lambda_con, kappa_con, lambda_2_con)
-                        ,  kappa_con = proportion_converence(kappa_con)
-                        , lambda_2_con = proportion_converence(lambda_2_con)
-                        , lambda_con = proportion_converence(lambda_con)
-                        , proportion = median(proportion)
-                        , date_first = median_cluster(date_first)
-                        , .groups = 'drop')
-                
-  
+summarised_results_all <- readRDS('sbv/third_infections/output/summarised_results_all.RDS')
 
-convergence_l2 <- (ggplot(summarised_results)
+convergence_l2 <- (ggplot(summarised_results_all)
                           + aes(x=pscale)
-                          + geom_line(aes(y=lambda_con, color="Lambda"))
-                          + geom_line(aes(y = kappa_con, color="Kappa"))
-                          + geom_line(aes(y = lambda_2_con, color="Lambda 2"))
-                          + geom_line(aes(y = convergence, color="All"))
-                          + labs(x="Scale", y='Proportion runs that converged', color='')
-                          + scale_color_manual(values = c("Kappa" = "blue", "Lambda" = "red", "Lambda 2" = "green", "All"="Pink"))
                           + styling_layers
+                   
+                          + geom_line(aes(y=lambda_con, color="lambda_con"))
+                          + geom_line(aes(y = kappa_con, color="kappa_con"))
+                          + geom_line(aes(y = lambda_2_con, color="lambda_2_con"))
+                          + geom_line(aes(y = convergence, color="convergence"))
+                          + scale_color_manual(values = c( lambda_con = "blue", kappa_con = "red",  lambda_2_con= "green", convergence = "pink")
+                                               , labels = c ( 
+                                                      kappa_con = parse(text = expression(kappa[2]))
+                                                    , lambda_con = parse(text = expression(lambda[2]))
+                                                    , lambda_2_con = parse(text = expression(lambda[2]^"'"))
+                                                    , convergence = 'All'
+                                                 )
+                                               , breaks = c('kappa_con', 'lambda_con', 'lambda_2_con', 'convergence' )
+                                               )
+                          + labs(x=expression(sigma[1]), y='Proportion runs that converged', color='')
                           + theme(plot.title = element_textbox_simple()
                                   , axis.title= element_text(size=9)
                                   , axis.text = element_text(size=9))
+                          + ylim(c(0,1))
 )
 
-ggsave('sbv/third_infections/plots/convergence_sbv.png')
+ggsave(convergence_l2, file ='sbv/third_infections/plots/convergence_sbv.png', width=5, height=5)
 
-#add column for converge
-final_RDS <- final_RDS %>% mutate(converges = (kappa_con<=1.1 & lambda_con<=1.1 & lambda_2_con<=1.1) )
+#plot for median of cluster above projection interval
+summarised_results <- readRDS('sbv/third_infections/output/summarised_results.RDS')
 
-excluded_results <- excludeResultsNonConverging('third', final_RDS )
+summarised_results_inc <- readRDS('sbv/third_infections/output/summarised_results_inc.RDS')
 
-result <- excluded_results %>%
-  group_by(pscale) %>%
-  summarise(count_kappa_lambda_lt_1.1 = sum(kappa_con <= 1.1 & lambda_con <= 1.1 & lambda_2_con<=1.1),
-            count_date_not_na = sum(!is.na(date_first) & kappa_con <= 1.1 & lambda_con <= 1.1 & lambda_2_con<=1.1)) %>%
-  mutate(specificity = 1 - (count_date_not_na / count_kappa_lambda_lt_1.1))
+summarised_results <- summarised_results %>% mutate(
+  pscale2 = 1, 
+)
+colnames(summarised_results)[1] ="pscale1"
 
-saveRDS(result, 'sbv/third_infections/output/specificity_matrix.RDS')
-write.csv(result,'sbv/third_infections/output/specificity_matrix.csv' )
+combined_results <- rbind(summarised_results, summarised_results_inc)
+#excluded_results_inc <- readRDS('sbv/third_infections/output/all_data_excluded_inc.RDS')
+
+cluster_third <- (ggplot(combined_results[combined_results$pscale1==2.8,]) 
+               + aes(x=pscale2, y=date_first_above) 
+               + geom_point()
+               + geom_segment(aes(x=pscale2, xend=pscale2, y=0, yend= date_first_above), linetype=2)
+               + labs(x=bquote(sigma[2]), y=paste0('Timing of first cluster above'))
+               + styling_layers
+               + theme(plot.title = element_textbox_simple()
+                       , axis.title= element_text(size=9)
+                       , axis.text = element_text(size=9))
+              # + xlim(1, 2)
+               + scale_x_continuous(breaks=c( 1, 1.2, 1.5, 2), limits=1:2)
+)
+
+ggsave(cluster_third,file = 'sbv/third_infections/plots/cluster_third_inc.png')
+
+
+proportion_third <- (ggplot(combined_results[combined_results$pscale1==2.8,]) 
+                  + aes(x=pscale2, y=proportion_above) 
+                  + geom_point()
+                  + geom_segment(aes(x=pscale2, xend=pscale2, y=0, yend= proportion_above), linetype=2)
+                  + labs(x=bquote(sigma[2]), y=paste0('Proportion above'))
+                  + styling_layers
+                  + theme(plot.title = element_textbox_simple()
+                          , axis.title= element_text(size=9)
+                          , axis.text = element_text(size=9))
+                  # + xlim(1, 2)
+                  + scale_x_continuous(breaks=c( 1, 1.2, 1.5, 2), limits=1:2)
+)
+
+ggsave(proportion_third, file = 'sbv/third_infections/plots/proportion_third_inc.png')
+
+combined_inc <- cluster_third / proportion_third + plot_annotation(tag_levels='A')
+ggsave(combined_inc, file ='sbv/third_infections/plots/combined_inc.tiff', dpi=300)
+
